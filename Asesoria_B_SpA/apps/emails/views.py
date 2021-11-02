126c9eeaeb9771f96cbae0615e4613e6cb8ca982
pablo andres torres labraña <pablo@MacBook-Pro-de-pablo.local>
2021-11-02 19:33:14 -0300

    chore(change_all): add new style

--- Asesoria_B_SpA/apps/emails/views.py
+++ Asesoria_B_SpA/apps/emails/views.py
@@ -6,15 +6,17 @@ from django.conf import settings
 def send_email(request):
 
     if request.method=="POST":
+        name=request.POST['fullname']
+        number=request.POST["number"]
         subject=request.POST['subject']
 
         message=request.POST['message'] + " " + request.POST['email']
 
         email_from=settings.EMAIL_HOST_USER
 
-        recipent_list=["bascunanlira@gmail.com"]
+        recipent_list=["pablolabrana2000@gmail.com"]
 
-        send_mail(subject, message, email_from, recipent_list)
+        send_mail(name,number,subject, message, email_from, recipent_list)
 
         return redirect('send_email')
     
